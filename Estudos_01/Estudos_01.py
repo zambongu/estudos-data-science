4f6b798964a3855a2b24bdf17d7acc2e845f3594
zambongu <guzambon@hotmail.com>
2025-10-28 19:38:56 -0300

    Estudos

--- Estudos_01/Estudos_01.py
+++ Estudos_01/Estudos_01.py
@@ -1,8 +1,11 @@
-"""Exemplo variavel global"""
+valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Lembre-se que as listas são baseadas em zero
+# print(f'O sétimo elemento da lista é {valores[6]}.')
+# print(f'E o conteúdo da lista completa: {valores}.')
+numeros_extenso = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
+lista_numeros = [valores, numeros_extenso]
+print(f'Lista de números: {lista_numeros}.')
+print(f'Primeiro elemento da lista de números: {lista_numeros[0][0]}-{lista_numeros[1][0]}.')
+print(f'Primeiro elemento da lista de números: {lista_numeros[0][0]}.')
+print(f'Ultimo elemento da lista de números: {lista_numeros[0][-1]}-{lista_numeros[1][-1]}.')
 
-codigo = "123321" # Variavel global
-
-def testar():
-    print(f'dentro da funcao testar(), codigo = {codigo}') # Acessando variavel global
-testar()
-print(f'fora da funcao testar(), codigo = {codigo}') # Acessando variavel global
