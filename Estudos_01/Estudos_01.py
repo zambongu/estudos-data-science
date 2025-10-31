feb1aaf34c5811e9e994cb8a1aea4490af898312
zambongu <guzambon@hotmail.com>
2025-10-31 19:49:55 -0300

    Friday_31_10

--- Estudos_01/Estudos_01.py
+++ Estudos_01/Estudos_01.py
@@ -1,11 +1,19 @@
-valores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# Lembre-se que as listas são baseadas em zero
-# print(f'O sétimo elemento da lista é {valores[6]}.')
-# print(f'E o conteúdo da lista completa: {valores}.')
-numeros_extenso = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
-lista_numeros = [valores, numeros_extenso]
-print(f'Lista de números: {lista_numeros}.')
-print(f'Primeiro elemento da lista de números: {lista_numeros[0][0]}-{lista_numeros[1][0]}.')
-print(f'Primeiro elemento da lista de números: {lista_numeros[0][0]}.')
-print(f'Ultimo elemento da lista de números: {lista_numeros[0][-1]}-{lista_numeros[1][-1]}.')
+var1 = 7
+var2 = 12
+var3 = 5
+if (var1 > var2) and (var1 < var3):
+   print(f'A mediana é {var1} e está armazenada em var1')
+elif (var2 > var1) and (var2 < var3):
+    print(f'A mediana é {var2} e está armazenada em var2')
+elif (var3 > var1) and (var3 < var2):
+    print(f'A mediana é {var3} e está armazenada em var3.')
 
+# var1 = 7
+# var2 = 12
+# var3 = 5
+# if (var1 > var2) and (var1 < var3):
+#     print(f'A mediana é {var1} e está armazenada em var1')
+# elif (var2 > var1) and (var2 < var3):
+#     print(f'A mediana é {var2} e está armazenada em var2')
+# elif (var3 > var1) and (var3 < var2):
+#     print(f'A mediana é {var3} e está armazenada em var3')
